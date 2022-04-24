878c72d52cd3b726ddb92a97c66744f8d824342e
Mario Kicherer <dev@kicherer.org>
2022-04-25 00:00:01 +0200

    query interesting props and attrs using a list and use print_key() for output

--- lsudev.c
+++ lsudev.c
@@ -20,6 +20,20 @@ void help(char **argv) {
 	fprintf(stdout, "  -U             show relevant parts for a udev rule\n");
 }
 
+const char *selected_props[] = {
+	"ID_MODEL",
+	"ID_BUS",
+	"ID_VENDOR_ID",
+	"ID_MODEL_ID",
+	"ID_NET_NAME_PATH",
+	0,
+};
+
+const char *selected_attrs[] = {
+	"address",
+	0,
+};
+
 void print_udev_rule(struct udev_device *dev, const char *prefix) {
 	const char *value, *subsystem;
 	
@@ -44,6 +58,17 @@ void print_udev_rule(struct udev_device *dev, const char *prefix) {
 			printf(", ENV{ID_USB_INTERFACE_NUM}==\"%s\"", value);
 		
 		printf(",\n");
+	}
+}
+
+#define RULE_ENV 0
+#define RULE_ATTR 1
+
+void print_key(const char *prefix, const char *key, const char *value, int env_or_attr, int show_udev_rule) {
+	if (show_udev_rule) {
+		printf("%s  %s{%s}==\"%s\"\n", prefix, env_or_attr?"ATTR":"ENV", key, value);
+	} else {
+		printf("%s  %s = %s\n", prefix, key, value);
 	}
 }
 
@@ -167,36 +192,34 @@ int main(int argc, char **argv) {
 				}
 				
 				if (show_all_properties) {
-					printf("%s  KERNEL=%s\n", prefix, udev_device_get_sysname(dev));
-					printf("%s  DEVTYPE=%s\n", prefix, udev_device_get_devtype(dev));
+					print_key(prefix, "KERNEL", udev_device_get_sysname(dev), RULE_ENV, show_udev_rule);
+					print_key(prefix, "DEVTYPE", udev_device_get_devtype(dev), RULE_ENV, show_udev_rule);
 					
 					dev_parent = udev_device_get_parent(dev);
 					if (dev_parent)
-						printf("%s  DRIVER=%s\n", prefix, udev_device_get_driver(dev_parent));
+						print_key(prefix, "DRIVER", udev_device_get_driver(dev_parent), RULE_ENV, show_udev_rule);
 				} else {
+					const char **iter;
+					
 					if (udev_device_get_devnode(dev))
 						printf("%s  /sys%s\n", prefix, udev_device_get_devpath(dev));
 					
-					value = udev_device_get_property_value(dev, "ID_MODEL");
-					if (value)
-						printf("%s  MODEL=%s\n", prefix, value);
-					
-					value = udev_device_get_property_value(dev, "ID_BUS");
-					if (
-						value && (
-							!strcmp(value, "usb")
-							|| !strcmp(value, "pci")
-							)
-						)
-					{
-						const char *vendor, *model;
-						vendor = udev_device_get_property_value(dev, "ID_VENDOR_ID");
-						if (vendor)
-							printf("%s  VENDOR_ID=%s\n", prefix, vendor);
+					iter = selected_props;
+					while (*iter) {
+						value = udev_device_get_property_value(dev, *iter);
+						if (value)
+							print_key(prefix, *iter, value, RULE_ENV, show_udev_rule);
 						
-						model = udev_device_get_property_value(dev, "ID_MODEL_ID");
-						if (model)
-							printf("%s  MODEL_ID=%s\n", prefix, model);
+						iter++;
+					}
+					
+					iter = selected_attrs;
+					while (*iter) {
+						value = udev_device_get_sysattr_value(dev, *iter);
+						if (value)
+							print_key(prefix, *iter, value, RULE_ATTR, show_udev_rule);
+						
+						iter++;
 					}
 				}
 				
@@ -204,7 +227,8 @@ int main(int argc, char **argv) {
 					udev_list_entry_foreach(entry, udev_device_get_properties_list_entry(dev)) {
 						key = udev_list_entry_get_name(entry);
 						value = udev_list_entry_get_value(entry);
-						printf("%s  %s = %s\n", prefix, key, value);
+						
+						print_key(prefix, key, value, RULE_ENV, show_udev_rule);
 					}
 					
 					udev_list_entry_foreach(entry, udev_device_get_sysattr_list_entry(dev)) {
@@ -218,7 +242,7 @@ int main(int argc, char **argv) {
 						if (!value)
 							continue;
 						
-						printf("%s  %s = %s\n", prefix, key, value);
+						print_key(prefix, key, value, RULE_ATTR, show_udev_rule);
 					}
 				}
 				
